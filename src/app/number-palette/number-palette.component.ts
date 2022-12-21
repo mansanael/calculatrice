d9ff3b2d458b507e4ecaa5b07e8733c209a27441
Mansa <magathnael@meltwater.org>
2022-12-21 23:31:01 +0000

    Version 0.0.1

--- src/app/number-palette/number-palette.component.ts
+++ src/app/number-palette/number-palette.component.ts
@@ -8,9 +8,45 @@ import { ChangeDetectionStrategy, Component, OnInit } from '@angular/core';
 })
 export class NumberPaletteComponent implements OnInit {
 
+  result = 0;
+  expression = '';
+
   constructor() { }
 
   ngOnInit(): void {
+  }
+
+  btnClicked(e:any){
+    console.log(e.target.innerText);
+    let value = e.target.innerText;
+
+    if (value == '=') {
+      this.result = eval(this.expression)
+    } else {
+      this.expression += e.target.innerText;
+    }
+
+    
+        
+    // } else if (value == '-'){
+    //   console.log('substraction');
+    // } else if(value == '/'){
+    //   console.log('division');
+
+    // }else if(value == 'x'){
+    //   console.log('multiplication');
+    // } else if(value == '='){
+    //   console.log(this.temp);
+    // }else {
+    //   this.temp = parseFloat(e.target.innerText) ;
+    //   console.log(eval("2+3*5"));
+    // }
+
+  }
+
+  cancel(){
+    this.expression = '';
+    this.result = 0;
   }
 
 }
